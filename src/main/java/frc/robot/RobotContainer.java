c55ff9105482849afbc2c6f3805e5d87890c22fd
G8rGears <team4973@gmail.com>
2025-02-17 12:14:11 -0800

    Working Pathplanner code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,12 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
+import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
@@ -73,6 +75,12 @@ public class RobotContainer {
 
     public Command getAutonomousCommand() {
         //return Commands.print("No autonomous command configured");
-        return runAuto1;
+        return runauto1;
+        // SequentialCommandGroup auto_commands = new SequentialCommandGroup(
+        //     Commands.print("Got Here!"),
+        //     new PathPlannerAuto("Test")
+        // );
+        // return auto_commands;
+        //return new PathPlannerAuto("Test");
     }
 }
